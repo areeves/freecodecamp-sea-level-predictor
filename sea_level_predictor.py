8f8620db0f48585e91444a4a6dffcd6a84b282cf
Andrew Reeves <areeves42@gmail.com>
2026-04-09 10:31:26 -0400

    plot line 2

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -6,20 +6,23 @@ from scipy.stats import linregress
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
-    year_col=df['Year']
-    csiro_col=df['CSIRO Adjusted Sea Level']
+    xcol = 'Year'
+    ycol = 'CSIRO Adjusted Sea Level'
 
     # Create scatter plot
     fig, ax = plt.subplots()
-    ax.scatter(x=year_col, y=csiro_col)
+    ax.scatter(x=df[xcol], y=df[ycol])
 
     # Create first line of best fit
-    year_range = np.arange(year_col.min(), 2051)
-    reg1 = linregress(year_col, csiro_col)
-    ax.plot(year_range, reg1.intercept + reg1.slope * year_range)
-
+    xrange = np.arange(df[xcol].min(), 2051)
+    reg1 = linregress(df[xcol], df[ycol])
+    ax.plot(xrange, reg1.intercept + reg1.slope * xrange)
 
     # Create second line of best fit
+    df2 = df[ df[xcol] >= 2000 ]
+    reg2 = linregress(df2[xcol], df2[ycol])
+    xrange2 = np.arange(df2[xcol].min(), 2051)
+    ax.plot(xrange2, reg2.intercept + reg2.slope * xrange2)
 
 
     # Add labels and title
